ff91cf3a7a275a8eaf56e1803e967a55c495a184
Tasha <tashingsworth@gmail.com>
2020-04-17 19:11:08 -0400

    created new header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,26 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function header() {
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const heading = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    date.textContent = 'MARCH 28, 2019';
+    heading.textContent = 'Lambda Times';
+    temp.textContent = '98°'
+
+    header.append(date);
+    header.append(heading);
+    header.append(temp);
+
+    return header;
+}
+
+const newHeader = document.querySelector('.header-container');
+
